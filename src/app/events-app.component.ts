e92d39d0787c22029632400b75f67b0848e864ab
Annie Huang <annie.hn@gmail.com>
2019-02-09 22:33:06 +1100

    update comment and add styles.css

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+/*
+The reason why /assets/ works is because in angular.json,
+in the projects/ng-fundamentals/architect/build section, it has
+            "assets": [
+              "src/favicon.ico",
+              "src/assets"
+            ],
+
+For styles and script use the below session:
+            "styles": [
+              "src/styles.css"
+            ],
+            "scripts": [],
+ */
 @Component({
   selector: 'events-app',
   template: `
