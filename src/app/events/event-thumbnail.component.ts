8584e11f2b741b4608d80117ea00db07e8254ae0
Annie Huang <annie.hn@gmail.com>
2019-02-10 21:28:13 +1100

    [style.XXX] or [ngStyle]. compared syntax with [ngClass]

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,7 +8,10 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
       <div>Date: {{event?.date}}</div>
       <!--<div [class.green]="event?.time === '8:00 am'" [ngSwitch]="event?.time">-->
       <!--<div [ngClass]="{green: event?.time === '8:00 am', bold: event?.time === '8:00 am'}" [ngSwitch]="event?.time">-->
-      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+      <!--<div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">-->
+      <!--<div [style.color]="event?.time === '8:00 am' ?  '#003300' : '#bbb'" [ngSwitch]="event?.time">-->
+      <!--<div [ngStyle]="{'color': event?.time === '8:00 am' ? '#003300' : '#bbb', 'font-weight': event?.time === '8:00 am' ? 'bold' : 'normal'}" [ngSwitch]="event?.time">-->
+      <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">
         Time: {{event?.time}}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -55,5 +58,12 @@ export class EventThumbnailComponent {
       return ['green', 'bold'];
     }
     return [];
+  }
+
+  getStartTimeStyle(): any {
+    if (this.event && this.event.time === '8:00 am') {
+      return {color: '#003300', 'font-weight': 'bold'};
+    }
+    return {};
   }
 }
