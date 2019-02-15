1a4c4ee72c4a318b921401dc5211ffa852ba0fa5
Annie Huang <annie.hn@gmail.com>
2019-02-15 21:49:33 +1100

    Passing in the id from the url params.

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 import {EventService} from '../shared/event.service';
 
 @Component({
@@ -8,12 +9,12 @@ import {EventService} from '../shared/event.service';
     .event-image {height: 100px;} 
   `]
 })
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit{
   event: any;
-  constructor(private eventService: EventService) {
+  constructor(private eventService: EventService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.event = this.eventService.getEvent(1);
+    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
   }
 }
