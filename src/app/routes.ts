4399bdf2f5a515e9c8240545198601c77c6a4339
Annie Huang <annie.hn@gmail.com>
2019-02-16 20:04:46 +1100

    Organizing Exports with Barrels so everything will referred from index.ts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,12 @@
 import {Routes} from '@angular/router';
-import {EventsListComponent} from './events/events-list.component';
-import {EventDetailsComponent} from './events/event-details/event-details.component';
-import {CreateEventComponent} from './events/create-event.component';
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventsListResolver
+} from './events/index';
 import {Error404Component} from './errors/404.component';
-import {EventRouteActivator} from './events/event-details/event-route-activator.service';
-import {EventsListResolver} from './events/events-list-resolver.service';
 
 // resolve: {event: EventsListResolver}
 //  Okay, so basically, what this is saying is, before resolving this route, call this event list resolver,
