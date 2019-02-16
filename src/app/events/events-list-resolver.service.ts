dc9b50cbc1d5cd0b8a0cc6cfdd1d1c218d85e561
Annie Huang <annie.hn@gmail.com>
2019-02-16 16:30:01 +1100

    Create events-list-resolver.service and set it as resolve: {event: EventsListResolver} for EventsListComponent

---
+++ src/app/events/events-list-resolver.service.ts
@@ -0,0 +1,15 @@
+import {Resolve} from '@angular/router';
+import {Injectable} from '@angular/core';
+import {EventService} from './shared/event.service';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class EventsListResolver implements Resolve<any> {
+  constructor(private eventService: EventService) {
+  }
+
+  // resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> | Promise<any> | any {
+  resolve() {
+    return this.eventService.getEvents().pipe(map(events => events));
+  }
+}
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,10 +4,14 @@ import {EventDetailsComponent} from './events/event-details/event-details.compon
 import {CreateEventComponent} from './events/create-event.component';
 import {Error404Component} from './errors/404.component';
 import {EventRouteActivator} from './events/event-details/event-route-activator.service';
+import {EventsListResolver} from './events/events-list-resolver.service';
 
+// resolve: {event: EventsListResolver}
+//  Okay, so basically, what this is saying is, before resolving this route, call this event list resolver,
+//  and when that resolver finishes and returns us some data, add this data to the route as a property named events.
 export const appRoutes: Routes = [
   { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
-  { path: 'events', component: EventsListComponent },
+  { path: 'events', component: EventsListComponent, resolve: {event: EventsListResolver} },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full'}
