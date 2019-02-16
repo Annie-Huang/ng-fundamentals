73e6bc15787d75927f16ac408c8f40fc043ab1b7
Annie Huang <annie.hn@gmail.com>
2019-02-16 23:01:45 +1100

    Add in the logic so when user first clicks into the login page, without entering anything, if he hovers over the Login button which is disable, he can see the error msg.

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -11,6 +11,7 @@ import {Router} from '@angular/router';
 export class LoginComponent {
   userName;
   password;
+  mouseoverLogin;
 
   constructor(private authService: AuthService, private router: Router) {
   }
