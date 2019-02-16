c0242870314a68c28906be3f7161941cafc36e2a
Annie Huang <annie.hn@gmail.com>
2019-02-16 22:11:07 +1100

    Create auth.service that use user.model interface. Make it public available in app.module. Call it from the login.component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,11 @@ import {NavbarComponent} from './nav/navbar.component';
 import {ToastrService} from './common/toastr.service';
 import {appRoutes} from './routes';
 import {Error404Component} from './errors/404.component';
+import {AuthService} from './user/auth.service';
 
+// AuthService: Well, that's because providers are shared across Angular modules. So if we add it in our app module,
+// it will be available for us to use here without having to register it as a provider in this module.    << UserModule.
+// It's worth nothing that while this is true for providers, it is not true for imports and declarations.
 @NgModule({
   imports: [
     BrowserModule,
@@ -36,6 +40,7 @@ import {Error404Component} from './errors/404.component';
     ToastrService,
     EventRouteActivator,
     EventsListResolver,
+    AuthService,
     {
       provide: 'canDeactivateCreateEvent',
       useValue: checkDirtyState
