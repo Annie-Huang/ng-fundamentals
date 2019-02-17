a96d458b8ae1a349fdd33a11ac594ed414b9ea9f
Annie Huang <annie.hn@gmail.com>
2019-02-17 21:46:21 +1100

    Display current user firstName|lastName from initial load. Implement cancel and Save button (submit)

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
+import {AuthService} from './auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html'
@@ -7,14 +9,25 @@ import {FormControl, FormGroup} from '@angular/forms';
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
 
+  constructor(private router: Router, private authService: AuthService) {
+  }
+
   ngOnInit(): void {
-    const firstName = new FormControl();
-    const lastName = new FormControl();
+    const firstName = new FormControl(this.authService.currentUser.firstName);
+    const lastName = new FormControl(this.authService.currentUser.lastName);
 
     this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
+      firstName,
+      lastName
     });
+  }
+
+  saveProfile(formValues) {
+    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+  }
+
+  cancel() {
+    this.router.navigate(['events']);
   }
 
 }
