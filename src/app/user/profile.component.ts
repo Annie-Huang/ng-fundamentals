381ebca347ee553d60eb9add4925d21cbb1934c8
Annie Huang <annie.hn@gmail.com>
2019-02-17 21:21:33 +1100

    Wired up firstName and lastName to formControlName and profileForm to formGroup.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,8 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   templateUrl: './profile.component.html'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  profileForm: FormGroup;
+
+  ngOnInit(): void {
+    const firstName = new FormControl();
+    const lastName = new FormControl();
+
+    this.profileForm = new FormGroup({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
 
 }
