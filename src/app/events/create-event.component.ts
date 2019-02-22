4dc40acbf78e408aa2925e58686f4cf5fc367b10
Annie Huang <annie.hn@gmail.com>
2019-02-22 21:43:48 +1100

    Add styles and saveEvent method.

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -2,13 +2,26 @@ import { Component } from '@angular/core';
 import {Router} from '@angular/router';
 
 @Component({
-  templateUrl: 'create-event.component.html'
+  templateUrl: 'create-event.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :-moz-placeholder { color:#999; }
+    .error :ms-input-placeholder { color: #999; }
+  `]
 })
 export class CreateEventComponent {
   isDirty = true;
 
   constructor(private router: Router) {
   }
+
+  saveEvent(formValues) {
+    console.log(formValues);
+  }
+
   cancel() {
     this.router.navigate(['/events']);
   }
