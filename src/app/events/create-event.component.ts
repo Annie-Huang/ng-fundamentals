0927446a63737c27d64010bbf4c0545b31672c7f
Annie Huang <annie.hn@gmail.com>
2019-03-12 21:52:49 +1100

    Create error style for each formControl and disabled Save button until the form is valid

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -8,8 +8,8 @@ import {EventService} from './shared/index';
     em {float:right; color:#E05C65; padding-left: 10px;}
     .error input {background-color:#E3C3C5;}
     .error ::-webkit-input-placeholder { color: #999; }
-    .error ::-moz-placeholder { color: #999; }
     .error :-moz-placeholder { color:#999; }
+    .error ::-moz-placeholder { color: #999; }
     .error :ms-input-placeholder { color: #999; }
   `]
 })
--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -3,7 +3,15 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {ISession} from '../shared';
 
 @Component({
-  templateUrl: './create-session.component.html'
+  templateUrl: './create-session.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input, .error select, .error textarea {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error :-moz-placeholder { color:#999; }
+    .error ::-moz-placeholder { color: #999; }
+    .error :ms-input-placeholder { color: #999; }
+  `]
 })
 export class CreateSessionComponent implements OnInit {
   newSessionForm: FormGroup;
