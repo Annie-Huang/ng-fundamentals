c00d370e2615ac9bee119d07885dca51dd61d8d4
Annie Huang <annie.hn@gmail.com>
2019-03-12 22:21:17 +1100

    moved restrictedWords as in seperate file (restricted-words.validator.ts) under shared folder

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {ISession} from '../shared';
+import {ISession, restrictedWords} from '../shared';
 
 @Component({
   templateUrl: './create-session.component.html',
@@ -26,7 +26,7 @@ export class CreateSessionComponent implements OnInit {
     this.presenter = new FormControl('', Validators.required);
     this.duration = new FormControl('', Validators.required);
     this.level = new FormControl('', Validators.required);
-    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])]);
+    this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), restrictedWords(['foo', 'bar'])]);
 
     this.newSessionForm = new FormGroup({
       name: this.name,
@@ -42,19 +42,7 @@ export class CreateSessionComponent implements OnInit {
   //     ? {restrictedWords: 'foo'}
   //     : null;
   // }
-  private restrictedWords(words) {
-    return (control: FormControl): {[key: string]: any} => {
-      if (!words) {return null; }
 
-      const invalidWords = words
-        .map(w => control.value.includes(w) ? w : null)
-        .filter(w => w != null);
-
-      return invalidWords && invalidWords.length > 0
-        ? {restrictedWords: invalidWords.join(', ')}
-        : null;
-    };
-  }
 
   saveSession(formValues) {
     const session: ISession = {
