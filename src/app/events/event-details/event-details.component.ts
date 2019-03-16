cca961fad2f4381260ea7d96c52d03a8bbf227cd
Annie Huang <annie.hn@gmail.com>
2019-03-16 11:04:18 +1100

    Hide sessions, as well as show create-session.component when addMode is clicked

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -8,15 +8,22 @@ import {IEvent} from '../shared';
   styles: [`
     .container {padding-left: 20px; padding-right: 20px;}
     .event-image {height: 100px;} 
+    a {cursor: pointer;}
   `]
 })
 export class EventDetailsComponent implements OnInit{
   // event: any;
   event: IEvent;
+  addMode: boolean;
+
   constructor(private eventService: EventService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
     this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+  }
+
+  addSession() {
+    this.addMode = true;
   }
 }
