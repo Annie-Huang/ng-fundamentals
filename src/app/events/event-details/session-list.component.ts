eaf88fa7124a3aca9375f044579d6f9dc6c313a3
Annie Huang <annie.hn@gmail.com>
2019-03-16 18:57:35 +1100

    Add [filterBy]="filterBy" @Input logic for session-list.component

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,33 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 import {ISession} from '../shared';
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
-export class SessionListComponent {
-  @Input() sessions:ISession[];
+export class SessionListComponent implements OnChanges {
+  @Input() sessions: ISession[];
+  @Input() filterBy: string;
+  visibleSessions: ISession[] = [];
+
+  // This ngOnChanges method is going to be called every time one of the input variables to this component gets a new value.
+  // ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(): void {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      // clone the sessions array
+      this.visibleSessions = this.sessions.slice(0);
+
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+
+  }
 }
