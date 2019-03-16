ab2cf6d74fdfb1f2a76cf47f0ecce9dc85c42d6a
Annie Huang <annie.hn@gmail.com>
2019-03-16 20:41:12 +1100

    Adding sortBy function

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,6 +8,7 @@ import {ISession} from '../shared';
 export class SessionListComponent implements OnChanges {
   @Input() sessions: ISession[];
   @Input() filterBy: string;
+  @Input() sortBy: string;
   visibleSessions: ISession[] = [];
 
   // This ngOnChanges method is going to be called every time one of the input variables to this component gets a new value.
@@ -15,9 +16,11 @@ export class SessionListComponent implements OnChanges {
   ngOnChanges(): void {
     if (this.sessions) {
       this.filterSessions(this.filterBy);
+      this.sortBy === 'name' ? this.visibleSessions.sort(sortByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
     }
   }
 
+  // You can do this filtering in here or event-details.component. Up to you.
   filterSessions(filter) {
     if (filter === 'all') {
       // clone the sessions array
@@ -30,4 +33,18 @@ export class SessionListComponent implements OnChanges {
     }
 
   }
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+  if (s1.name > s2.name) {
+    return 1;
+  } else if (s1.name === s2.name) {
+    return 0;
+  } else {
+    return -1;
+  }
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return s2.voters.length - s1.voters.length;
 }
