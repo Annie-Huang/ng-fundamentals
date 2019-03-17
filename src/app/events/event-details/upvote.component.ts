8bccb2999ddb6d399035aef6ec97bd39b354e6ed
Annie Huang <annie.hn@gmail.com>
2019-03-17 15:54:07 +1100

    Use input setters in order to create a derived value from an input property. @Input() set voted(val) {...}

--- src/app/events/event-details/upvote.component.ts
+++ src/app/events/event-details/upvote.component.ts
@@ -3,13 +3,14 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'upvote',
-  styleUrls: './upvote.component.css',
+  styleUrls: ['./upvote.component.css'],
   template: `
     <div class="votingWidgetContainer pointable" (click)="onClick()">
       <div class="well votingWidget">
         <div class="votingButton">
-          <i *ngIf="voted" class="glyphicon glyphicon-heart"></i>
-          <i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>
+          <!--<i *ngIf="voted" class="glyphicon glyphicon-heart"></i>-->
+          <!--<i *ngIf="!voted" class="glyphicon glyphicon-heart-empty"></i>-->
+          <i class="glyphicon glyphicon-heart" [style.color]="iconColor"></i>
         </div>
         <div class="badge badge-inverse votingCount">
           <div>{{count}}</div>
@@ -20,8 +21,17 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class UpvoteComponent {
   @Input() count: number;
-  @Input() voted: boolean;
+
+  // @Input() voted: boolean;
+
+  // use input setters in order to create a derived value from an input property.
+  @Input() set voted(val) {
+    this.iconColor = val ? 'red' : 'white';
+  }
+
   @Output() vote = new EventEmitter();
+
+  iconColor: string;
 
   onClick() {
     this.vote.emit({});
